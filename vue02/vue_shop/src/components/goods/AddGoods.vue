f4a6db898813e59fbdbcd806288e7d5504c2206e
chenBW <861754186@qq.com>
2021-04-15 16:03:00 +0800

    阻止切换

--- vue02/vue_shop/src/components/goods/AddGoods.vue
+++ vue02/vue_shop/src/components/goods/AddGoods.vue
@@ -26,19 +26,48 @@
        <el-step title="完成"></el-step>
      </el-steps>
 
-     <!--添加商品-->
-     <el-tabs v-model="activeIndex" :tab-position="'left'" style="height: 200px;">
-       <el-tab-pane label="基本信息" name="0">基本信息</el-tab-pane>
-       <el-tab-pane label="商品参数" name="1">商品参数</el-tab-pane>
-       <el-tab-pane label="商品属性" name="2">商品属性</el-tab-pane>
-       <el-tab-pane label="商品图片" name="3">商品图片</el-tab-pane>
-       <el-tab-pane label="商品内容" name="4">商品内容</el-tab-pane>
-     </el-tabs>
 
+     <!--设置 form 表单-->
+
+     <el-form :model="addGoodsForm" :rules="addGoodsFormRules" ref="addGoodsFormRef"
+              label-width="100px" label-position="top">
+
+       <!--添加商品-->
+       <el-tabs v-model="activeIndex" :tab-position="'left'" :before-leave="beforeTabLeave">
+         <el-tab-pane label="基本信息" name="0">
+
+           <el-form-item label="商品名称" prop="goods_name">
+             <el-input v-model="addGoodsForm.goods_name"></el-input>
+           </el-form-item>
+           <el-form-item label="商品价格" prop="goods_price">
+             <el-input v-model="addGoodsForm.goods_price" type="number"></el-input>
+           </el-form-item>
+           <el-form-item label="商品重量" prop="goods_weight">
+             <el-input v-model="addGoodsForm.goods_weight" type="number"></el-input>
+           </el-form-item>
+           <el-form-item label="商品数量" prop="goods_number">
+             <el-input v-model="addGoodsForm.goods_number"></el-input>
+           </el-form-item>
+
+           <el-form-item label="商品分类" prop="goods_cat">
+             <el-cascader
+               :props="categoryProps"
+               :options="categoryList"
+               v-model="addGoodsForm.goods_cat"
+               @change="parentCategoryChange">
+             </el-cascader>
+           </el-form-item>
+
+         </el-tab-pane>
+         <el-tab-pane label="商品参数" name="1">商品参数</el-tab-pane>
+         <el-tab-pane label="商品属性" name="2">商品属性</el-tab-pane>
+         <el-tab-pane label="商品图片" name="3">商品图片</el-tab-pane>
+         <el-tab-pane label="商品内容" name="4">商品内容</el-tab-pane>
+       </el-tabs>
+
+     </el-form>
 
    </el-card>
-
-
 
   </div>
     
@@ -49,15 +78,78 @@
     name: "AddGoods",
     created(){
 
+      this.getCategoriesList()
     },
     data(){
       return {
 
         activeIndex:'0',
+        addGoodsForm:{
+          goods_name:'',
+          goods_price:0,
+          goods_weight:0,
+          goods_number:0,
+          goods_cat:[]
+
+        },
+        addGoodsFormRules:{
+          goods_name:[
+            { required: true, message: '请输入商品名', trigger: 'blur' },
+          ],
+          goods_price:[
+            { required: true, message: '请输入商品价格', trigger: 'blur' },
+          ],
+          goods_weight:[
+            { required: true, message: '请输入商品重量', trigger: 'blur' },
+          ],
+          goods_number:[
+            { required: true, message: '请输入商品重量', trigger: 'blur' },
+
+          ],
+          goods_cat:[
+            { required: true, message: '请输入商品重量', trigger: 'blur' },
+          ]
+        },
+        categoryList:[],
+        categoryProps:{
+          value:'cat_id',
+          label:'cat_name',
+          children:'children',
+          expandTrigger:'hover',
+          // checkStrictly:true
+        },
+        selectedCategoryKyes:[]
 
       }
     },
-    methods:{
+    methods: {
+
+      async getCategoriesList(){
+
+        const {data:res} = await this.$http.get('categories')
+        if (res.meta.status !== 200){
+          return this.$message.error(res.meta.msg)
+        }
+        this.categoryList = res.data
+
+        console.log(res)
+      },
+      //级联选择器变化,会出发这个函数
+      parentCategoryChange(){
+
+        if(this.addGoodsForm.goods_cat.length !== 3){
+          this.addGoodsForm.goods_cat = []
+        }
+      },
+      beforeTabLeave(activeName,oldActiveName){
+
+        //要选中的名字,离开的名字
+        if(oldActiveName === '0' && this.addGoodsForm.goods_cat.length !== 3){
+          this.$message.error('请先选择商品分类!')
+          return false
+        }
+
+      }
 
     }
   }
