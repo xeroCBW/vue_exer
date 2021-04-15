fa35867830488d6a829dce28ada258775a47bf28
chenBW <861754186@qq.com>
2021-04-15 17:24:22 +0800

    设置图片上传的请求头

--- vue02/vue_shop/src/components/goods/AddGoods.vue
+++ vue02/vue_shop/src/components/goods/AddGoods.vue
@@ -94,6 +94,7 @@
              :action="uploadURL"
              :on-preview="handlePreview"
              :on-remove="handleRemove"
+             :headers="headerObj"
              list-type="picture">
              <el-button size="small" type="primary">点击上传</el-button>
              <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
@@ -160,7 +161,11 @@
         },
         manyTableData:[],
         onlyTableData:[],
-        uploadURL:"https://www.liulongbin.top:8888/api/private/v1/upload"
+        uploadURL:"https://www.liulongbin.top:8888/api/private/v1/upload",
+        //图片上传的请求头
+        headerObj:{
+          Authorization:window.sessionStorage.getItem('token')
+        }
 
 
       }
